e1137a7978988cc874ffe5a6a83e413c1ec8053e
Riley <91571914+rilstrats@users.noreply.github.com>
2024-10-22 09:23:36 -0600

    colored printing works

---
+++ linux.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[40m"      /* Black */
+#define RED     "\033[41m"      /* Red */
+#define GREEN   "\033[42m"      /* Green */
+#define YELLOW  "\033[43m"      /* Yellow */
+#define BLUE    "\033[44m"      /* Blue */
+#define MAGENTA "\033[45m"      /* Magenta */
+#define CYAN    "\033[46m"      /* Cyan */
+#define WHITE   "\033[47m"      /* White */
+
+int main() {
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << RESET << endl;
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << RESET << endl;
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << RESET << endl;
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << RESET << endl;
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << RESET << endl;
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << BLACK << "  ";
+	cout << WHITE<< "  ";
+	cout << RESET << endl;
+}
